89441873d09ff5ed55c4622e3ac846e1593f6c0c
xterminal86 <xterminal86@gmail.com>
2023-05-03 00:10:43 +0300

    [sprint6]
    - WIP (cont.)

--- sprint6/DFS/main.py
+++ sprint6/DFS/main.py
@@ -76,7 +76,7 @@ def BullshitSolution():
   vertices = int(line[0]);
   edges    = int(line[1]);
 
-  graph = {};
+  graph = [ None ] * (vertices + 1);
 
   colors = [0] * (vertices + 1);
 
@@ -85,20 +85,21 @@ def BullshitSolution():
     f = int(line[0]);
     t = int(line[1]);
 
-    if f not in graph:
+    if graph[f] == None:
       graph[f] = [ t ];
     else:
       graph[f].append(t);
 
-    if t not in graph:
+    if graph[t] == None:
       graph[t] = [ f ];
     else:
       graph[t].append(f);
 
-  for k in graph.keys():
-    graph[k].sort();
-
   start = int(input().rstrip());
+
+  for item in graph:
+    if item != None:
+      item.sort();
 
   #print(graph, start);
 
@@ -119,7 +120,7 @@ def BullshitSolution():
 
     found = False;
 
-    if v in graph:
+    if graph[v] != None:
       for item in graph[v]:
         if colors[item] == 0:
           found = True;
