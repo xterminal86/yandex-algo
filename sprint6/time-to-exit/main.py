819c188588493eef098abe48b4c7ffcae7e8a297
xterminal86 <xterminal86@gmail.com>
2023-05-03 20:48:55 +0300

    [sprint6]
    - solved H

--- sprint6/time-to-exit/main.py
+++ sprint6/time-to-exit/main.py
@@ -2,39 +2,7 @@
 
 ################################################################################
 
-Counter = 0;
-Entry = [];
-Leave = [];
-
-def DFS(v, graph, colors):
-  global Counter;
-  global Entry;
-  global Leave;
-
-  Counter += 1;
-  Entry[v] = Counter;
-
-  colors[v] = 1;
-
-  if graph[v] != None:
-    for item in graph[v]:
-      if colors[item] == 0:
-        DFS(item, graph, colors);
-
-  Counter += 1;
-  Leave[v] = Counter;
-  colors[v] = 2;
-
-  #print("  entry:", Entry);
-  #print("  leave:", Leave);
-
-################################################################################
-
 def main():
-  global Entry;
-  global Leave;
-  global Counter;
-
   line = input().rstrip().split();
   vertices = int(line[0]);
   edges    = int(line[1]);
@@ -57,14 +25,45 @@ def main():
 
   for item in graph:
     if item != None:
-      item.sort();
+      item.sort(reverse=True);
 
   #print(graph);
 
-  DFS(1, graph, colors);
+  toProcess = [ 1 ];
+
+  entry = [ 0 ] * (vertices + 1);
+  leave = [ 0 ] * (vertices + 1);
+
+  cnt = 0;
+
+  while len(toProcess) != 0:
+    #print("toProcess: ", toProcess);
+
+    v = toProcess.pop();
+
+    #print("got ", v);
+
+    if colors[v] == 0:
+      colors[v] = 1;
+      #print(f"  setting entry { cnt } for { v }");
+      entry[v] = cnt;
+      cnt += 1;
+      toProcess.append(v);
+
+      if graph[v] != None:
+        for item in graph[v]:
+          if colors[item] == 0:
+            #print("  adding", item);
+            toProcess.append(item);
+
+    elif colors[v] == 1:
+      colors[v] = 2;
+      leave[v] = cnt;
+      #print(f"  setting leave { cnt } for { v }");
+      cnt += 1;
 
   for i in range(1, vertices + 1):
-    print(Entry[i] - 1, Leave[i] - 1);
+    print(entry[i], leave[i]);
 
 ################################################################################
 
