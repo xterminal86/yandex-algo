7e302f1b00ee2220048fc1a17d15ab2349eb5ea0
xterminal86 <xterminal86@gmail.com>
2023-05-16 21:27:22 +0300

    [sprint7]
    - solved final A

---
+++ sprint7/final/same-sums/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys;
+
+################################################################################
+
+def main():
+  n = int(input().rstrip());
+
+  scores = list( map(int, sys.stdin.readline().rstrip().split()) );
+
+  list.sort(scores);
+
+  total = sum(scores);
+
+  limit = total // 2;
+
+  dp = [ False ] * (limit + 1);
+
+  dp[0] = True;
+
+  for score in scores:
+    #print(f"new score = { score }");
+    for i in range(limit, score - 1, -1):
+      #print(f"  i = { i }, score = { score }, i - score = { i - score }");
+      if dp[limit] == True:
+        print("True");
+        return;
+
+      dp[i] = (dp[i] or dp[i - score]);
+      #print("  ", dp);
+
+  if dp[limit] == True:
+    print("True");
+  else:
+    print("False");
+
+################################################################################
+
+if __name__ == "__main__":
+  main();
