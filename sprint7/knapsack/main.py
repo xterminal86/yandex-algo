15c741e58e4613af007b72e67105fb1dfe0b04c3
xterminal86 <xterminal86@gmail.com>
2023-05-17 20:13:38 +0300

    [sprint7]
    - solved M

--- sprint7/knapsack/main.py
+++ sprint7/knapsack/main.py
@@ -27,39 +27,42 @@ def main():
     weights[i] = m;
     values[i]  = c;
 
-  print("weights = ", weights);
-  print("values  = ", values);
-  print("-"*80);
+  #print("weights = ", weights);
+  #print("values  = ", values);
+  #print("-"*80);
 
   for i in range(n + 1):
+    #print(f"i = { i }");
     for w in range(capacity + 1):
       if (i == 0) or (w == 0):
         dp[i][w] = 0;
       elif weights[i] <= w:
-        print(f"  i = { i } w = { w } weights[i] = { weights[i] } (w - weights[i]) = { w - weights[i] }");
-        print(f"  values[i] = { values[i] }");
+        #print(f"  i = { i } w = { w } weights[i] = { weights[i] } (w - weights[i]) = { w - weights[i] }");
+        #print(f"  values[i] = { values[i] }");
         dp[i][w] = max(values[i] + dp[i - 1][ w - weights[i] ], dp[i - 1][w]);
       else:
         dp[i][w] = dp[i - 1][w];
 
-    print(f"i = { i }");
-    PrintMatrix(dp);
-    print("-"*80);
+    #PrintMatrix(dp);
+    #print("-"*80);
 
   #PrintMatrix(dp);
 
   i = n;
   j = capacity;
 
+  ans = [];
+
   while (i > 0) and (j > 0):
-    print(f"  [{ i }][{ j }] = { dp[i][j] }");
     if (dp[i][j] == dp[i - 1][j]):
-      print(f"{ i } = 0");
       i += -1;
     else:
-      print(f"{ i } = 1");
-      i += -1;
+      ans.append(i);
       j = j - weights[i];
+      i += -1;
+
+  print(len(ans));
+  print(*ans);
 
 ################################################################################
 
